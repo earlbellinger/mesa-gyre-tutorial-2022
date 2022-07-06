1821bf93d6e799fee60fc281137f3adf6da32dae
Earl Patrick Bellinger <earlbellinger@gmail.com>
2022-07-06 23:06:28 +0200

    notebook

--- src/install_MESA.sh
+++ src/install_MESA.sh
@@ -8,12 +8,10 @@
 #################
 ### Variables ###
 #################
-# Adjust these if you want a different version 
-MESA_VER=22.05.1
-SDK_VER=22.6.1
-
-# Adjust this if you want to use more than one processor
-export OMP_NUM_THREADS=1 
+MESA_VER=22.05.1         # Adjust these if you want a different version 
+SDK_VER=22.6.1           # ...but check the zenodo link below 
+export OMP_NUM_THREADS=1 # Adjust this if you want to use more threads 
+add_to_bashrc=1          # set to zero if you don't want your bashrc to change 
 
 ######################
 ### Pre-requisites ###
@@ -34,11 +32,9 @@ mkdir $SDK_REV
 tar xvfz "$SDK_REV".tar.gz -C "$SDK_REV"
 ln -sfn "$SDK_REV"/mesasdk mesasdk
 export MESASDK_ROOT=$(pwd)/mesasdk
-echo export MESASDK_ROOT="$MESASDK_ROOT" >> ~/.bashrc 
 
 ## Run SDK
 source $MESASDK_ROOT/bin/mesasdk_init.sh
-echo source "$MESASDK_ROOT"/bin/mesasdk_init.sh >> ~/.bashrc 
 
 #################################
 ### Download and install MESA ###
@@ -49,9 +45,20 @@ wget https://zenodo.org/record/6547951/files/"$MESA_REV".zip
 unzip "$MESA_REV".zip
 ln -sfn $(pwd)/"$MESA_REV" mesa
 export MESA_DIR=$(pwd)/mesa
-echo export MESA_DIR="$MESA_DIR" >> ~/.bashrc
-echo export OMP_NUM_THREADS="$OMP_NUM_THREADS" >> ~/.bashrc
 
 ## Install MESA
 cd $MESA_DIR
 ./install
+
+## Install GYRE
+export GYRE_DIR=$MESA_DIR/gyre/gyre
+cd $GYRE_DIR
+make
+
+if [ add_to_bashrc == 1 ]; then
+    echo export MESASDK_ROOT="$MESASDK_ROOT" >> ~/.bashrc 
+    echo export MESA_DIR="$MESA_DIR" >> ~/.bashrc
+    echo export GYRE_DIR=$MESA_DIR/gyre/gyre >> ~/.bashrc 
+    echo export OMP_NUM_THREADS="$OMP_NUM_THREADS" >> ~/.bashrc
+    echo source "$MESASDK_ROOT"/bin/mesasdk_init.sh >> ~/.bashrc 
+fi
